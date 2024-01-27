15f5b7a3b79d96e1b608ba458b98310b49efc919
Eltorretaz <eltorretaz470@gmail.com>
2024-01-27 16:47:19 -0400

    jaja god

---
+++ practice3.rb
@@ -0,0 +1,7 @@
+movies = { mario_bros: 2023,
+           aquaman: 2018,
+           man_of_steel: 2013 }
+
+puts movies[:mario_bros]
+puts movies[:aquaman]
+puts movies[:man_of_steel]
---
+++ practice4.rb
@@ -0,0 +1,5 @@
+dates = [2006, 2007, 1990,]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
